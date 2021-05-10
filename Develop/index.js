d6c98cd1d8cc4e629eae10e7bb99d4458a1f9c4b
chdonovan <chrisdonovan311@gmail.com>
2021-05-10 13:51:25 -0500

    user questions enter into index

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,70 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const util = require("util");
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+function promptUser(){
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "projectTitle",
+            message: "Enter the Project Title",
+        },
+        {   
+            type: "input",
+            name: "description",
+            message: "Enter the Project Description",
+        },
+        {
+            type: "input",
+            name: "instalation",
+            message: "Instalation instructions here",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "enter project usage info here",
+
+        },
+        {
+            type: "input",
+            name: "contributuon",
+            message:"enter contribution instructions",
+        },
+        {
+            type: "input",
+            name: "testing",
+            message: "enter testing info here",
+        
+        },
+        {
+            type: "list",
+            name:"license",
+            message: "Choose the appropriate License for your project",
+            choices: [
+                "Apache",
+                "Acdemic",
+                "GNU",
+                "ISC",
+                "MIT",
+                "Mozilla",
+                "Open"
+            ]
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "enter your github username here",
+
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "enter your email address here"
+        }
+    ]);
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
